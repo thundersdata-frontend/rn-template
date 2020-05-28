b552ed42a4cf15df5a32d3e4d56942af1247f85b
hss <huangshanshan@thundersdata.com>
2020-05-28 16:07:54 +0800

    refactor: 升级navigation依赖解决tab报错问题以及修复tab中图标文字不对称的问题

--- src/stacks/tab/index.tsx
+++ src/stacks/tab/index.tsx
@@ -3,7 +3,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Test1 from '../../screens/test1';
 import Test2 from '../../screens/test2';
-import { Text, View } from 'react-native';
+import { Text } from 'react-native';
 import { Size, Color } from '../../config';
 import { Icon } from '@ant-design/react-native';
 import { IconNames } from '@ant-design/react-native/lib/icon';
@@ -29,59 +29,24 @@ export default function TabStack() {
   ];
 
   return (
-    <Tab.Navigator
-      tabBarOptions={{
-        style: {},
-        labelStyle: {
-          marginBottom: Size.px(1)
-        },
-        tabStyle: {}
-      }}
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused }) => {
-          const matchTab = tabItems.find(item => item.name === route.name);
-          return (
-            <View style={{ width: 24, height: 24, margin: 0 }}>
-              <Icon
-                name={matchTab?.icon as IconNames}
-                size={24}
-                color={focused ? Color.primary : Color.middleTextColor}
-              />
-              {matchTab?.badgeCount && (
-                <View
-                  style={{
-                    // On React Native < 0.57 overflow outside of parent will not work on Android, see https://git.io/fhLJ8
-                    position: 'absolute',
-                    right: -8,
-                    top: -1,
-                    backgroundColor: '#FA3A22',
-                    borderRadius: 8,
-                    width: 16,
-                    height: 16,
-                    justifyContent: 'center',
-                    alignItems: 'center'
-                  }}>
-                  <Text style={{ color: 'white', fontSize: 10, fontWeight: '400' }}>{matchTab?.badgeCount}</Text>
-                </View>
-              )}
-            </View>
-          );
-        },
-        tabBarLabel: ({ focused }) => {
-          return (
-            <Text
-              style={{
-                color: focused ? Color.primary : Color.middleTextColor,
-                fontWeight: '400',
-                fontSize: Size.px(10)
-              }}>
-              {tabItems.find(item => item.name === route.name)?.label}
-            </Text>
-          );
-        }
-      })}>
+    <Tab.Navigator initialRouteName="HomePage" lazy={true}>
       {tabItems.map(item => (
-        <Tab.Screen key={item.name} name={item.name} component={item.component} />
+        <Tab.Screen
+          key={item.name}
+          name={item.name}
+          component={item.component}
+          options={{
+            title: item.label,
+            tabBarLabel: ({ focused }) => (
+              <Text style={{ color: focused ? Color.mainTextColor : Color.middleTextColor, fontSize: Size.px(10) }}>
+                {item?.label}
+              </Text>
+            ),
+            tabBarIcon: ({ focused }) => (
+              <Icon name={item.icon as IconNames} size={24} color={focused ? Color.primary : Color.mainTextColor} />
+            )
+          }}
+        />
       ))}
     </Tab.Navigator>
   );
