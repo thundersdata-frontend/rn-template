15c07b6737184af330ea42a4869dd082c0bd0c84
chj_damon <chjdamon@gmail.com>
2021-03-23 15:44:41 +0800

    feat: 集成异常捕获功能

--- index.js
+++ index.js
@@ -1,9 +1,47 @@
-/**
- * @format
- */
+import {Alert, AppRegistry} from 'react-native';
+import {
+  setJSExceptionHandler,
+  setNativeExceptionHandler,
+} from 'react-native-exception-handler';
+import RNRestart from 'react-native-restart';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './src/App';
 import {name as appName} from './app.json';
+
+/**
+ * 未捕获的JS异常
+ */
+setJSExceptionHandler((error, isFatal) => {
+  if (isFatal) {
+    Alert.alert(
+      '未知异常',
+      `
+        Error: ${isFatal ? 'Fatal:' : ''} ${error.name} ${error.message}
+        APP需要被重启
+      `,
+      [
+        {
+          text: '重启',
+          onPress: () => {
+            RNRestart.Restart();
+          },
+        },
+      ],
+    );
+  } else {
+    console.log(error); // So that we can see it in the ADB logs in case of Android if needed
+  }
+}, false);
+
+/**
+ * 未捕获的原生异常
+ */
+setNativeExceptionHandler(
+  exceptionString => {
+    console.log(exceptionString);
+  },
+  false,
+  true,
+);
 
 AppRegistry.registerComponent(appName, () => App);
---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import React, {useEffect} from 'react';
+import {Button, SafeAreaView, Text, View} from 'react-native';
+import RNBootSplash from 'react-native-bootsplash';
+
+import {API_URL} from '@env';
+import ErrorHandler from 'components/ErrorHandler';
+
+export default function App() {
+  useEffect(() => {
+    const init = async () => {
+      // …do multiple sync or async tasks
+    };
+
+    init().finally(async () => {
+      await RNBootSplash.hide({fade: true});
+      console.log('Bootsplash has been hidden successfully');
+    });
+  }, []);
+
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>API_URL: {API_URL}</Text>
+        <Text>hello, rn template</Text>
+        <ErrorHandler>
+          <ErrorDemo />
+        </ErrorHandler>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+function ErrorDemo() {
+  return (
+    <View>
+      <Text>{test}</Text>
+    </View>
+  );
+}
