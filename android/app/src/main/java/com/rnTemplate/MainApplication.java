bde2684dd55c15aedf858b5319a248168f19fe71
chj_damon <chjdamon@gmail.com>
2021-05-11 16:47:43 +0800

    fix: 修复安卓启动报错的问题

--- android/app/src/main/java/com/rnTemplate/MainApplication.java
+++ android/app/src/main/java/com/rnTemplate/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.reactnativemmkv.MmkvModulePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
