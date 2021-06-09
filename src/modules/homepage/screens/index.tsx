ee4eeb96f159ae61bc8fb8cba531caa35993c3fa
chj_damon <chjdamon@gmail.com>
2021-06-09 11:56:08 +0800

    feat: 补充导致项目运行失败的示例

--- src/modules/homepage/screens/index.tsx
+++ src/modules/homepage/screens/index.tsx
@@ -1,46 +1,31 @@
-import { useState } from 'react';
-import { Text, View } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { StackNavigationProp } from '@react-navigation/stack';
-import useSWR from 'swr';
-import { useUpdateAtom } from 'jotai/utils';
-import { Button, WhiteSpace } from '@td-design/react-native';
-
-import { authAtom } from 'modules/auth/authService';
+/* eslint-disable react-native/no-color-literals */
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
+import { Button, WhiteSpace, Input } from '@td-design/react-native';
 import { Container } from 'components/Container';
 
-const { fetch, url } = API.authorization.resource.listResource;
+export function Homepage() {
+  const x = useSharedValue(0);
 
-export function Homepage({ navigation }: { navigation: StackNavigationProp<MainStackParamList, 'Homepage'> }) {
-  const updateAuth = useUpdateAtom(authAtom);
+  const rotateStyle = useAnimatedStyle(() => ({
+    transform: [
+      {
+        translateX: x.value,
+      },
+    ],
+  }));
 
-  const [shouldFetch, setShouldFetch] = useState(false);
-  const {} = useSWR(shouldFetch ? url : null, fetch);
-
-  const logout = () => {
-    updateAuth({ signedIn: false });
-  };
-
-  const getData = () => {
-    setShouldFetch(true);
+  const handleRotate = () => {
+    x.value = withTiming(100);
   };
 
   return (
     <Container hasHeader={false}>
-      <View>
-        <Text>我是首页</Text>
-      </View>
-      <Button onPress={logout} title="退出登录" />
+      <Animated.View style={[{ backgroundColor: '#0189fb', width: 50, height: 50 }, rotateStyle]} />
       <WhiteSpace />
-      <Button onPress={getData} title="调接口" />
+      <Button title="rotate" onPress={handleRotate} />
       <WhiteSpace />
-      <Button onPress={() => AsyncStorage.setItem('token', '123')} title="保存数据" />
-      <WhiteSpace />
-      <Button onPress={() => console.log(AsyncStorage.getItem('token'))} title="获取数据" />
-      <WhiteSpace />
-      <Button onPress={() => navigation.navigate('Mine')} title="去我的页面" />
-      <WhiteSpace />
-      {/* <Input /> */}
+      {/** uncomment the next line will report error */}
+      <Input />
       {/* <SearchBar /> */}
       {/* <NoticeBar text="我是通知我是通知我是通知我是通知我是通知我是通知22222" /> */}
     </Container>
