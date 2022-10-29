45a198ac5037185fc3b2eb9a2123d77d3b71a492
chj_damon <chjdamon@gmail.com>
2022-10-29 16:03:12 +0800

    feat: 初始化0.69.6版本

--- android/app/src/main/java/com/rnTemplate/newarchitecture/MainApplicationReactNativeHost.java
+++ android/app/src/main/java/com/rnTemplate/newarchitecture/MainApplicationReactNativeHost.java
@@ -18,6 +18,7 @@ import com.facebook.react.fabric.ComponentFactory;
 import com.facebook.react.fabric.CoreComponentsRegistry;
 import com.facebook.react.fabric.EmptyReactNativeConfig;
 import com.facebook.react.fabric.FabricJSIModuleProvider;
+import com.facebook.react.fabric.ReactNativeConfig;
 import com.facebook.react.uimanager.ViewManagerRegistry;
 import com.rntemplate.BuildConfig;
 import com.rntemplate.newarchitecture.components.MainComponentsRegistry;
@@ -93,7 +94,7 @@ public class MainApplicationReactNativeHost extends ReactNativeHost {
                 return new FabricJSIModuleProvider(
                     reactApplicationContext,
                     componentFactory,
-                    new EmptyReactNativeConfig(),
+                    ReactNativeConfig.DEFAULT_CONFIG,
                     viewManagerRegistry);
               }
             });
