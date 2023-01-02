2e1b4bf4883432ed80d23408de2e9054b718cf32
chj_damon <chjdamon@gmail.com>
2023-01-02 11:14:45 +0800

    fix: 修复一些bug

--- src/modules/homepage/screens/index.tsx
+++ src/modules/homepage/screens/index.tsx
@@ -1,6 +1,6 @@
 import NiceModal from '@ebay/nice-modal-react';
 import { NavigationProp, useNavigation } from '@react-navigation/native';
-import { Button, Center } from '@td-design/react-native';
+import { Button, Center, WhiteSpace } from '@td-design/react-native';
 import { Container } from 'components';
 import TestModal from 'modals/TestModal';
 import codePush from 'react-native-code-push';
@@ -13,13 +13,18 @@ export function Homepage() {
     <Container>
       <Center>
         <Button title="局部共享数据示例" onPress={() => navigation.navigate('LocalModelDemo')} />
+        <WhiteSpace />
         <Button title="FlashList示例" onPress={() => navigation.navigate('FlashListDemo')} />
-        <Button title="瀑布流示例" onPress={() => navigation.navigate('WaterfallListDemo')} />
-        <Button title="通讯录示例" onPress={() => navigation.navigate('ContactsDemo')} />
+        <WhiteSpace />
+        <Button title="瀑布流示例333" onPress={() => navigation.navigate('WaterfallListDemo')} />
+        <WhiteSpace />
+        <Button title="通讯录示例444" onPress={() => navigation.navigate('ContactsDemo')} />
+        <WhiteSpace />
         <Button
           title="弹窗测试"
           onPress={() => NiceModal.show(TestModal, { content: '我是内容', position: 'center' })}
         />
+        <WhiteSpace />
         <Button
           title="热更新测试"
           onPress={() =>
