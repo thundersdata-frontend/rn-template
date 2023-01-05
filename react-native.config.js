42cb630ca87be9ac3a181e95a969ab891cf6f823
chj_damon <chjdamon@gmail.com>
2023-01-05 14:55:45 +0800

    fix: 优化code-push相关配置

--- react-native.config.js
+++ react-native.config.js
@@ -4,11 +4,4 @@ module.exports = {
     android: {}, // grouped into "project"
   },
   assets: ['./assets/fonts/'], // stays the same
-  dependencies: {
-    "react-native-code-push": {
-      platforms: {
-        android: null
-      }
-    }
-  }
 };
